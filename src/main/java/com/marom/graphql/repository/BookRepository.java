732bc29828531e60a2c1a01139a4448c30159997
marom <maro.muszynski@gmail.com>
2020-05-30 13:21:31 +0100

    add missing repository annotation

--- src/main/java/com/marom/graphql/repository/BookRepository.java
+++ src/main/java/com/marom/graphql/repository/BookRepository.java
@@ -2,7 +2,9 @@ package com.marom.graphql.repository;
 
 import com.marom.graphql.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, String> {
 
 }
