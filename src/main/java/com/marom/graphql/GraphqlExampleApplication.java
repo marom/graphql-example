91666f05a30e3f9b25821aa8f9f1a1e0b813380c
marom <maro.muszynski@gmail.com>
2020-06-07 13:32:53 +0100

    redefine GraphQL's default error handler, reate a bean of type GraphQLErrorHandler to replace the default error handler implementation

--- src/main/java/com/marom/graphql/GraphqlExampleApplication.java
+++ src/main/java/com/marom/graphql/GraphqlExampleApplication.java
@@ -1,13 +1,48 @@
 package com.marom.graphql;
 
+import com.marom.graphql.exception.GraphQLErrorAdapter;
+import graphql.ExceptionWhileDataFetching;
+import graphql.GraphQLError;
+import graphql.servlet.GraphQLErrorHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class GraphqlExampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GraphqlExampleApplication.class, args);
+    }
+
+    @Bean
+    public GraphQLErrorHandler errorHandler() {
+        return new GraphQLErrorHandler() {
+            @Override
+            public List<GraphQLError> processErrors(List<GraphQLError> errors) {
+                List<GraphQLError> clientErrors = errors.stream()
+                        .filter(this::isClientError)
+                        .collect(Collectors.toList());
+
+                List<GraphQLError> serverErrors = errors.stream()
+                        .filter(e -> !isClientError(e))
+                        .map(GraphQLErrorAdapter::new)
+                        .collect(Collectors.toList());
+
+                List<GraphQLError> e = new ArrayList<>();
+                e.addAll(clientErrors);
+                e.addAll(serverErrors);
+                return e;
+            }
+
+            protected boolean isClientError(GraphQLError error) {
+                return !(error instanceof ExceptionWhileDataFetching || error instanceof Throwable);
+            }
+        };
     }
 
 }
