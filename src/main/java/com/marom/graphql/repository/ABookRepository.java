43e41ae9088ef273655b29116f028b88146ce8c9
marom <maro.muszynski@gmail.com>
2020-06-07 15:38:48 +0100

    use a jpa repository

--- src/main/java/com/marom/graphql/repository/ABookRepository.java
+++ src/main/java/com/marom/graphql/repository/ABookRepository.java
@@ -1,7 +1,9 @@
 package com.marom.graphql.repository;
 
 import com.marom.graphql.model.ABook;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ABookRepository extends CrudRepository<ABook, Long> {
+@Repository
+public interface ABookRepository extends JpaRepository<ABook, Long> {
 }
--- src/main/java/com/marom/graphql/repository/AuthorRepository.java
+++ src/main/java/com/marom/graphql/repository/AuthorRepository.java
@@ -1,7 +1,9 @@
 package com.marom.graphql.repository;
 
 import com.marom.graphql.model.Author;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface AuthorRepository extends CrudRepository<Author, Long> {
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
 }
