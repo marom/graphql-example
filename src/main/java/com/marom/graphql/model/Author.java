1c08b00ce7c8915034f8ba3e4c1250855432e5ec
marom <maro.muszynski@gmail.com>
2020-06-07 13:38:56 +0100

    define column names and create a custom constructor for an Author

--- src/main/java/com/marom/graphql/model/Author.java
+++ src/main/java/com/marom/graphql/model/Author.java
@@ -16,13 +16,22 @@ import javax.persistence.*;
 public class Author {
 
     @Id
+    @Column(name="author_id", nullable = false)
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(name="author_first_name", nullable = false)
     private String firstName;
+
+    @Column(name="author_last_name", nullable = false)
     private String lastName;
 
     public Author(Long id) {
         this.id = id;
     }
 
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
